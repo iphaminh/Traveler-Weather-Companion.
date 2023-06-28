ae47df710067c0320d274f4e58219a82cfb76d41
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-06-27 23:14:00 -0400

    adding understanding comments

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
 // Global variables
 var searchHistory = [];
 var weatherApiRootUrl = 'https://api.openweathermap.org';
-var weatherApiKey = 'd91f911bcf2c0f925fb6535547a5ddc9';
+var weatherApiKey = 'b3fa3d3b239e314dd54de4c4686696b3';
+
 
 // DOM element references
-var searchForm = document.querySelector('#search-form');
-var searchInput = document.querySelector('#search-input');
-var todayContainer = document.querySelector('#today');
-var forecastContainer = document.querySelector('#forecast');
-var searchHistoryContainer = document.querySelector('#history');
+var searchForm = document.querySelector('#search-form'); //1
+var searchInput = document.querySelector('#search-input'); //2
+var todayContainer = document.querySelector('#today'); //3
+var forecastContainer = document.querySelector('#forecast'); //4
+var searchHistoryContainer = document.querySelector('#history'); //5
 
 // Add timezone plugins to day.js
-dayjs.extend(window.dayjs_plugin_utc);
-dayjs.extend(window.dayjs_plugin_timezone);
+dayjs.extend(window.dayjs_plugin_utc); //sent user timezone to utc then convert back
+dayjs.extend(window.dayjs_plugin_timezone); // plugin to set on their users timezone
 
 // Function to display the search history list.
 function renderSearchHistory() {
   searchHistoryContainer.innerHTML = '';
 
   // Start at end of history array and count down to show the most recent at the top.
-  for (var i = searchHistory.length - 1; i >= 0; i--) {
-    var btn = document.createElement('button');
-    btn.setAttribute('type', 'button');
-    btn.setAttribute('aria-controls', 'today forecast');
-    btn.classList.add('history-btn', 'btn-history');
+  for (var i = searchHistory.length - 1; i >= 0; i--) { // dont get this much from  the last element in an array is the array's length minus 1.
+    var btn = document.createElement('button');  //make btn
+    btn.setAttribute('type', 'button'); 
+    btn.setAttribute('aria-controls', 'today forecast'); // Accessible Rich Internet Applications  IDs "today" and "forecast".
+    btn.classList.add('history-btn', 'btn-history'); // class list is able to add more than 1 classes
 
     // `data-search` allows access to city name when click handler is invoked
     btn.setAttribute('data-search', searchHistory[i]);
@@ -33,22 +34,22 @@ function renderSearchHistory() {
 }
 
 // Function to update history in local storage then updates displayed history.
-function appendToHistory(search) {
+function appendToHistory(search) { //update the search history and to fetch the weather data for that city
   // If there is no search term return the function
-  if (searchHistory.indexOf(search) !== -1) {
-    return;
+  if (searchHistory.indexOf(search) !== -1) { 
+    return; //This line checks if the search term is already in the searchHistory array. If it is, the function returns immediately and does nothing
   }
-  searchHistory.push(search);
+  searchHistory.push(search); // new search
 
-  localStorage.setItem('search-history', JSON.stringify(searchHistory));
+  localStorage.setItem('search-history', JSON.stringify(searchHistory)); //save and convert array to string bc local storage always does string
   renderSearchHistory();
 }
 
 // Function to get search history from local storage
-function initSearchHistory() {
+function initSearchHistory() { //load the history
   var storedHistory = localStorage.getItem('search-history');
   if (storedHistory) {
-    searchHistory = JSON.parse(storedHistory);
+    searchHistory = JSON.parse(storedHistory); // if it does have some then convert back to array i wish they should have the universal term to use so we dont have to convert all the time
   }
   renderSearchHistory();
 }
@@ -60,9 +61,10 @@ function renderCurrentWeather(city, weather) {
   var tempF = weather.main.temp;
   var windMph = weather.wind.speed;
   var humidity = weather.main.humidity;
-  var iconUrl = `https://openweathermap.org/img/w/${weather.weather[0].icon}.png`;
-  var iconDescription = weather.weather[0].description || weather[0].main;
+  var iconUrl = `https://openweathermap.org/img/w/${weather.weather[0].icon}.png`; //This constructs the URL for the weather icon image.
+  var iconDescription = weather.weather[0].description || weather[0].main; //This gets the description of the weather icon.
 
+  //creating new elements
   var card = document.createElement('div');
   var cardBody = document.createElement('div');
   var heading = document.createElement('h2');
@@ -70,7 +72,7 @@ function renderCurrentWeather(city, weather) {
   var tempEl = document.createElement('p');
   var windEl = document.createElement('p');
   var humidityEl = document.createElement('p');
-
+//adding classes
   card.setAttribute('class', 'card');
   cardBody.setAttribute('class', 'card-body');
   card.append(cardBody);
@@ -85,40 +87,40 @@ function renderCurrentWeather(city, weather) {
   weatherIcon.setAttribute('alt', iconDescription);
   weatherIcon.setAttribute('class', 'weather-img');
   heading.append(weatherIcon);
-  tempEl.textContent = `Temp: ${tempF}°F`;
-  windEl.textContent = `Wind: ${windMph} MPH`;
-  humidityEl.textContent = `Humidity: ${humidity} %`;
-  cardBody.append(heading, tempEl, windEl, humidityEl);
+  tempEl.textContent = `Temp: ${tempF}°F`; // element to the temperature.
+  windEl.textContent = `Wind: ${windMph} MPH`; // element to the wind.
+  humidityEl.textContent = `Humidity: ${humidity} %`; // same
+  cardBody.append(heading, tempEl, windEl, humidityEl); // append child elements all of these to cardBody
 
-  todayContainer.innerHTML = '';
-  todayContainer.append(card);
+  todayContainer.innerHTML = ''; //empty content
+  todayContainer.append(card); // append card to todayContainer
 }
 
 // Function to display a forecast card given an object from open weather api
 // daily forecast.
-function renderForecastCard(forecast) {
+function renderForecastCard(forecast) { //name parameter forecast and show the weather forecast data for a single day.
   // variables for data from api
-  var iconUrl = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
-  var iconDescription = forecast.weather[0].description;
-  var tempF = forecast.main.temp;
+  var iconUrl = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`; // same with current weather data
+  var iconDescription = forecast.weather[0].description; //icon description?? how
+  var tempF = forecast.main.temp; 
   var humidity = forecast.main.humidity;
   var windMph = forecast.wind.speed;
 
   // Create elements for a card
-  var col = document.createElement('div');
-  var card = document.createElement('div');
-  var cardBody = document.createElement('div');
-  var cardTitle = document.createElement('h5');
-  var weatherIcon = document.createElement('img');
-  var tempEl = document.createElement('p');
+  var col = document.createElement('div'); //create div
+  var card = document.createElement('div'); //create div
+  var cardBody = document.createElement('div'); //create div
+  var cardTitle = document.createElement('h5'); //h5 element
+  var weatherIcon = document.createElement('img'); //iconimage
+  var tempEl = document.createElement('p'); //p tag
   var windEl = document.createElement('p');
   var humidityEl = document.createElement('p');
 
-  col.append(card);
-  card.append(cardBody);
-  cardBody.append(cardTitle, weatherIcon, tempEl, windEl, humidityEl);
+  col.append(card); //card becomes a child element of col, and it will be nested inside col in the HTML structure.
+  card.append(cardBody); //similar concept
+  cardBody.append(cardTitle, weatherIcon, tempEl, windEl, humidityEl); //adding elements to cardBody
 
-  col.setAttribute('class', 'col-md');
+  col.setAttribute('class', 'col-md'); //no number SOOOO automatically take up equal space within its container, along with any other col-md elements.
   col.classList.add('five-day-card');
   card.setAttribute('class', 'card bg-primary h-100 text-white');
   cardBody.setAttribute('class', 'card-body p-2');
